22418aa59dee5fa8ba4b77458dde078044524166
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-08-25 14:26:48 +0530

    first commit

---
+++ Basics of JavaScript/index.js
@@ -0,0 +1,28 @@
+// /*
+//  * First Program  - Show Hello Geeks on consle
+//  * Understand what is console
+//  */
+
+// console.log("Hello Geeks!!");
+// console.log("My name is Saurav verma.");
+
+// console.log(7 + 36);
+
+// // Variables
+
+// var message = "Hello, GeeksforGeeks!!";
+// console.log(message);
+// console.log(message);
+// console.log(message);
+// console.log(message);
+// message = "My Name is Saurav";
+// console.log(message);
+
+let textMessage = "Javascript is the best in the world!";
+
+console.log(textMessage);
+
+const number = 45;
+console.log(number);
+number = 48;
+console.log(number);
