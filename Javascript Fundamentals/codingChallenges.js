ebb2f5d34278b38ee3a417e2bed4f95b3131b99d
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-08-27 02:38:28 +0530

    Done few Javascript coding questions

--- Javascript Fundamentals/codingChallenges.js
+++ Javascript Fundamentals/codingChallenges.js
@@ -1,13 +1,15 @@
-const firsDigit = (num) => {
-  const len = String(num).length;
-  for (let i = 0; i < len - 1; i++) {
-    num = Math.floor(num / 10);
-  }
-  return Number(num);
-};
+function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
+  const monthlyRate = ratePerHour * 8 * 22;
+  const monthlyRateWithDiscount = monthlyRate * (1 - discount);
+  const totalMonths = Math.floor(numDays / 22);
+  const extraDays = numDays % 22;
+  const extraDaysCost = extraDays * ratePerHour * 8;
+  const finalCost = Math.ceil(
+    totalMonths * monthlyRateWithDiscount + extraDaysCost
+  );
+  return finalCost;
+}
 
-console.log(firsDigit(2356));
-console.log(firsDigit(916));
-console.log(firsDigit(123));
-console.log(firsDigit(20));
-console.log(firsDigit(1));
+// console.log(priceWithMonthlyDiscount(89, 230, 0.42)); // => 97972
+// console.log(priceWithMonthlyDiscount(16, 70, 0)); // => 8960
+console.log(priceWithMonthlyDiscount(16, 130, 0.15)); // => 14528
