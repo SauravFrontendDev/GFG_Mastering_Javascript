c59df523aad4ccaba385c6a94d15876c2aaba078
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-08-27 22:10:41 +0530

    for loops in js

---
+++ Javascript Fundamentals/loops.js
@@ -0,0 +1,33 @@
+/**
+ * Star Pattern
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *
+ * * * * * *
+ * * * * * * *
+ */
+
+const symbol = "* ";
+
+// console.log(symbol.repeat(1));
+// console.log(symbol.repeat(2));
+// console.log(symbol.repeat(3));
+// console.log(symbol.repeat(4));
+// console.log(symbol.repeat(5));
+// console.log(symbol.repeat(6));
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(symbol.repeat(i));
+// }
+// for (let i = 9; i > 0; i--) {
+//   console.log(symbol.repeat(i));
+// }
+
+const instructorName = "Prakash Narsinghrao Sakari";
+
+console.log(instructorName.length);
+for (let i = 0; i < instructorName.length; i++) {
+  console.log(instructorName[i]);
+}
---
+++ Javascript Fundamentals/nullishCoalescing.js
@@ -0,0 +1,7 @@
+let firstName = 0;
+
+// console.log(firstName ?? "Saurav");
+
+const a = 0;
+console.log(a || 1);
+console.log(a ?? 1);
